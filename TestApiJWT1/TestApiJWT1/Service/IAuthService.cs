46b85a73317584b9bff6690b3465ab0a54428880
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2023-09-19 15:18:36 -0700

    First commit

---
+++ TestApiJWT1/TestApiJWT1/Service/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TestApiJWT1.Models;
+
+namespace TestApiJWT1.Service
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+    }
+}
